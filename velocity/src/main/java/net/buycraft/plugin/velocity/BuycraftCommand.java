05322280dca9b4b24755a7d515c61216b1252e01
Tony S <59147271+TheJokerDev@users.noreply.github.com>
2022-09-02 22:52:25 -0600

    Support to Velocity
    Added support to latest release of Velocity's Proxy.

--- velocity/src/main/java/net/buycraft/plugin/velocity/BuycraftCommand.java
+++ velocity/src/main/java/net/buycraft/plugin/velocity/BuycraftCommand.java
@@ -1,17 +1,17 @@
 package net.buycraft.plugin.velocity;
 
-import com.velocitypowered.api.command.Command;
 import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.command.SimpleCommand;
 import net.buycraft.plugin.velocity.command.Subcommand;
-import net.kyori.text.TextComponent;
-import net.kyori.text.format.TextColor;
-import net.kyori.text.format.TextDecoration;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class BuycraftCommand implements Command {
+public class BuycraftCommand implements SimpleCommand {
     private final Map<String, Subcommand> subcommandMap = new LinkedHashMap<>();
     private final BuycraftPlugin plugin;
 
@@ -19,10 +19,23 @@ public class BuycraftCommand implements Command {
         this.plugin = plugin;
     }
 
+    private void showHelp(CommandSource sender) {
+        sender.sendMessage(Component.text(plugin.getI18n().get("usage")).color(TextColor.color(36,157,159)).decoration(TextDecoration.BOLD, true));
+        for (Map.Entry<String, Subcommand> entry : subcommandMap.entrySet()) {
+            sender.sendMessage(Component.text("/tebex " + entry.getKey()).color(TextColor.color(0, 255, 0)).append(Component.text(": " + entry.getValue().getDescription())));
+        }
+    }
+
+    public Map<String, Subcommand> getSubcommandMap() {
+        return this.subcommandMap;
+    }
+
     @Override
-    public void execute(CommandSource sender, String[] args) {
+    public void execute(Invocation invocation) {
+        CommandSource sender = invocation.source();
+        String[] args = invocation.arguments();
         if (!sender.hasPermission("buycraft.admin")) {
-            sender.sendMessage(TextComponent.of(plugin.getI18n().get("no_permission")).color(TextColor.RED));
+            sender.sendMessage(Component.text(plugin.getI18n().get("no_permission")).color(TextColor.color(255, 0, 0)));
             return;
         }
 
@@ -40,16 +53,5 @@ public class BuycraftCommand implements Command {
         }
 
         showHelp(sender);
-    }
-
-    private void showHelp(CommandSource sender) {
-        sender.sendMessage(TextComponent.of(plugin.getI18n().get("usage")).color(TextColor.DARK_AQUA).decoration(TextDecoration.BOLD, true));
-        for (Map.Entry<String, Subcommand> entry : subcommandMap.entrySet()) {
-            sender.sendMessage(TextComponent.of("/tebex " + entry.getKey()).color(TextColor.GREEN).append(TextComponent.of(": " + entry.getValue().getDescription())));
-        }
-    }
-
-    public Map<String, Subcommand> getSubcommandMap() {
-        return this.subcommandMap;
     }
 }
